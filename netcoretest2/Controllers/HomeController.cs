fcab7d1145f6ecc513d57e397d5ee1bfca157b8d
Jordy <Jordy.tak@gmail.com>
2018-09-24 03:04:28 +0200

    some change

---
+++ netcoretest2/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netcoretest2.Controllers
+{
+    public class HomeController
+    {
+        public string index() {
+
+
+            return "hello from the homecontroller";
+        }
+    }
+}
